4d2bd1809e2b8fc1da933e2acbf2f4aa3fe6a440
TauriSaar <94620796+TauriSaar@users.noreply.github.com>
2024-02-24 14:08:07 +0200

    3. weather-test - otsing linna järgi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { createMockServer } from './createMockServer';
+import config from './config';
+
+if(process.env.NODE_ENV === 'development') {
+  createMockServer();
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const inputChangeHandler = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const buttonClickHandler = () => {
+    const apiKey = config.apiKey;
+
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${apiKey}`)
+      .then((result) => result.json())
+      .then((cities) => {
+        setSearchResults(
+          cities.map((city) => ({
+            name: city.name,
+            country: city.country,
+            lat: city.lat,
+            lon: city.lon
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <input type="text" data-testid="search-input" onChange={inputChangeHandler} />
+      <button data-testid="search-button" onClick={buttonClickHandler}>
+        Search
+      </button>
+  
+      <div data-testid="search-results">
+        {searchResults.map((city) => (
+          <div key={`${city.lat}-${city.con}`}>{city.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { createMockServer } from './createMockServer';
+
 describe('Weather Application', () => {
-  test('renders weather application title', () => {
+  let server;
+  beforeEach(() => {
+    server = createMockServer();
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  if('renders weather application title', () => {
     render(<App />);
     const linkElement = screen.getByText(/Weather Application/i);
     expect(linkElement).toBeInTheDocument();
-  });	
+  });
+
+  if ('shows city search results', async () => {
+    render(<App />);
+
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Tartu');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await awaitFor(() => expect(screen.getAllByText(/Tartu/i)).length).toEqual(5);
+  });
 });
